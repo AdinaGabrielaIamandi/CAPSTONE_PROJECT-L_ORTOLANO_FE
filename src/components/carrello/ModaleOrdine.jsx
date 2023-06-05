6456e9b3b475d665bbb4db75e54ae9c82eacd7f8
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-06-05 17:07:18 +0200

    grazie

--- src/components/carrello/ModaleOrdine.jsx
+++ src/components/carrello/ModaleOrdine.jsx
@@ -4,7 +4,7 @@ export const ModaleOrdine = () => {
   return (
     <Container className="m-5 p-5 text-center">
       <h3>Il tuo ordine è stato preso in carico!</h3>
-      <h4>Grazie per averci scleto!</h4>
+      <h4>Grazie per averci scleto, a breve riceverà una mail con tutte le informazioni</h4>
     </Container>
   );
 };
